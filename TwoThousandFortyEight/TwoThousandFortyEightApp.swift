0a27d88824fdd3e55ccf70fb9e08df68143908ee
syk <663992442@qq.com>
2023-07-21 16:25:05 +0800

    Initial Commit

---
+++ TwoThousandFortyEight/TwoThousandFortyEightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoThousandFortyEightApp.swift
+//  TwoThousandFortyEight
+//
+//  Created by ma c on 2023/7/21.
+//
+
+import SwiftUI
+
+@main
+struct TwoThousandFortyEightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
